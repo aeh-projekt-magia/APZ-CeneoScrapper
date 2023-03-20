8fef09b7f32a392da41d4bdccbcee426ffd1ccf4
Stealthedcactus <stealthedcactus@gmail.com>
2023-03-20 21:04:07 +0100

    update

---
+++ flask_app/app/models/models.py
@@ -0,0 +1,25 @@
+from app.extensions import db
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Text)
+
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Text)
+    author = db.Column(db.Text)
+    recommendation = db.Column(db.Text)
+    stars = db.Column(db.Text)
+    content = db.Column(db.Text)
+    publish_date = db.Column(db.Text)
+    purchase_date = db.Column(db.Text)
+    useful = db.Column(db.Text)
+    useless = db.Column(db.Text)
+    pros = db.Column(db.Text)
+    cons = db.Column(db.Text)
+    review_id = db.Column(db.Text)
+
+    def __repr__(self):
+        return f'<Review no {self.review_id}: {self.author} - {self.content}>'
