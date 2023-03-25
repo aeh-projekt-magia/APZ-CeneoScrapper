5aefe70d911555dab27f0a78df86ed98ff116ecc
Konrad Swiercz <konrad.swiercz@hotmail.com>
2023-03-25 17:38:42 +0100

    Wszystko działa, baza podpięta

--- flask_app/config.py
+++ flask_app/config.py
@@ -6,5 +6,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    SQLALCHEMY_DATABASE_URI = "postgresql://hello:hello12@db:5432/hello_db"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
