c0b3384db20719fb4a1b89e9fbd5c24e1c86de83
Bartolomeo624 <ratajczakbartlomiej0@gmail.com>
2023-03-27 20:22:18 +0200

    PM-20 uncomment the app init

--- flask_app/app/__init__.py
+++ flask_app/app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-# from app.extensions import db
+from app.extensions import db
 from flask_app.config import Config
 
 
@@ -8,12 +8,12 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-    # db.init_app(app)
+    db.init_app(app)
 
-    # from app.controllers.main import bp as main_bp
-    # app.register_blueprint(main_bp)
-    #
-    # from app.controllers.products import bp as products_bp
-    # app.register_blueprint(products_bp, url_prefix='/products')
+    from app.controllers.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    from app.controllers.products import bp as products_bp
+    app.register_blueprint(products_bp, url_prefix='/products')
 
     return app
