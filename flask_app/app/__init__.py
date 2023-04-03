b68ae70b38d08325d17a25aefc0ec862f743c0c1
stealthedcactus <stealthedcactus@gmail.com>
2023-04-03 11:51:41 +0200

    Added server error handling 404, 500

--- flask_app/app/__init__.py
+++ flask_app/app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from app.extensions import db, bcrypt, migrate, login_manager
 
@@ -35,4 +35,20 @@ def create_app(config_class=DevelopmentConfig):
     app.register_blueprint(products_bp, url_prefix='/products')
 
     # app.shell_context_processor({'app': app, 'db': db})
+
+
+    """No use, since every page is user login required"""
+    # @app.errorhandler(401)
+    # def unauthorized_page(error):
+    #     return render_template("errors/401.html"), 401
+
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template("errors/404.html"), 404
+
+
+    @app.errorhandler(500)
+    def server_error_page(error):
+        return render_template("errors/500.html"), 500
+
     return app
---
+++ flask_app/tests/unit/test_home_page.py
@@ -0,0 +1,11 @@
+def test_home_page(client):
+    """Check if home page works and returns expected values"""
+    response = client.get("/", follow_redirects=True)
+    assert response.status_code == 200
+    assert b'Please log in to access this page.' in response.data
+
+
+def test_home_page_aliases(client):
+    """Check if other page works and returns expected values"""
+    assert b'Please log in to access this page.' in client.get("/", follow_redirects=True).data
+    assert b'Please log in to access this page.' in client.get("/products/", follow_redirects=True).data
