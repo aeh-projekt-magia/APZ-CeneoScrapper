98cea5b270463ae9735620296d4defebc4da4e8e
stealthedcactus <stealthedcactus@gmail.com>
2023-05-09 23:45:49 +0200

    subscriptions query added

--- flask_app/app/controllers/subscriptions/routes.py
+++ flask_app/app/controllers/subscriptions/routes.py
@@ -13,12 +13,20 @@ from app.services.product_service import ProductService
 def index():
     """Wyświetlenie zasubskrybowanych produktów"""
     # TODO:subscriptions/routes - Dodać repozytorium
-    page = request.args.get("page", 1, type=int)
-    
-    products_to_show = SubscriptionService.get_user_subscriptions_paginate(
-        user_id=current_user.id, page=page, pages=15)
 
-            
+    page = request.args.get("page", 1, type=int)
+
+    query_name = request.args.get("query_name") 
+    print(f"{query_name=}")
+    if query_name is None or "":
+        products_to_show = SubscriptionService.get_user_subscriptions(
+            user_id=current_user.id
+        ).paginate(page=page, per_page=2)
+    else:
+        products_to_show = SubscriptionService.get_user_subscriptions_by_name(
+            user_id=current_user.id, product_name=query_name
+        ).paginate(page=page, per_page=5)
+
     return render_template("subscriptions/index.html", products=products_to_show)
 
 
@@ -46,11 +54,11 @@ def single_subscription_view(product_id):
         price_history=product_price_history,
     )
 
+
 @bp.route("/<int:product_id>/update", methods=["GET", "POST"])
 @login_required
 @confirmed_user_required
 def single_subscription_update(product_id):
-
     if not SubscriptionService.check_if_subscribed(
         user_id=current_user.id, product_id=product_id
     ):
@@ -64,23 +72,28 @@ def single_subscription_update(product_id):
 
     # Create default values for form fields
     form = SubscriptionUpdate(
-        notification_frequency = subscription.notification_frequency, 
+        notification_frequency=subscription.notification_frequency,
         notify_on_price_change="Yes" if subscription.notify_on_price_change else "No",
-        send_notification="Yes" if subscription.send_notification else "No"
+        send_notification="Yes" if subscription.send_notification else "No",
     )
     if request.method == "POST":
         if form.validate_on_submit():
-            SubscriptionService.update_subscription(subscription=subscription, update={
-                "notification_frequency":form.notification_frequency.data,
-                "notify_on_price_change": form.notify_on_price_change.data,
-                "send_notification":form.send_notification.data})
-            return redirect(url_for("subscriptions.single_subscription_view", product_id=product_id))
-
+            SubscriptionService.update_subscription(
+                subscription=subscription,
+                update={
+                    "notification_frequency": form.notification_frequency.data,
+                    "notify_on_price_change": form.notify_on_price_change.data,
+                    "send_notification": form.send_notification.data,
+                },
+            )
+            return redirect(
+                url_for("subscriptions.single_subscription_view", product_id=product_id)
+            )
 
     return render_template(
         "subscriptions/single_subscription_update.html",
         product=product,
         subscription=subscription,
-        price_history=product_price_history, 
-        form=form
+        price_history=product_price_history,
+        form=form,
     )
