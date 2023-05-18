86f7a83edb50b62650e6245a26a32f9f6876bf04
= <=>
2023-05-18 20:28:31 +0200

    Reports ready

--- flask_app/locust.sh
+++ flask_app/locust.sh
@@ -4,10 +4,11 @@ do
     trap "echo Exited!; exit;" SIGINT SIGTERM
     filename_var="locust_reports/report$(date "+%Y%M%d_%H%M%S").html"
     locust \
-    --host http://127.0.0.1:5000 \
+    --host http://192.168.1.107:5001 \
     --users $xx \
     --spawn-rate 2.5 \
     --autostart \
+    --headless \
     --html $filename_var \
     --run-time 10m
 done
